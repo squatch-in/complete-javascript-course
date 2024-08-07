9162cf1405357236bcca870655c417082109c5cf
squatch-in <jvwyatt7@gmail.com>
2024-08-07 16:06:24 -0500

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let welcome = ("hi");  
+ console.log( 40 + 27 - 8);
+console.log('jones');
+console.log(23);
+
+let firstName = "Jacob";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
