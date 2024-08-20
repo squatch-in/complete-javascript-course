da68d4704241bcb9e28ef34f3bfbea62275b89d2
Jacob <jvwyatt7@gmail.com>
2024-08-19 21:48:02 -0500

    double check

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -137,3 +137,5 @@ const calcAverage = (score1, score2, score3) => {
      const scoreDolphins = calcAverage(44, 23, 71);
      const scoreKoalas = calcAverage(65, 54, 49);
      console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+     //hello
