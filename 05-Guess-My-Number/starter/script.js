2ad65651f85e588086f8fb6b7821c7d36e0e8476
Jacob <jvwyatt7@gmail.com>
2024-09-13 20:58:53 -0500

    dom ;)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.number'));
+document.querySelector('.number').textContent = 10;
+console.log(document.querySelector('.label-score').textContent);
+document.querySelector('.score').textContent = 10;
+console.log(document.querySelector('.label-highscore').textContent);
+document.querySelector('.highscore').textContent = 15;
+document.querySelector('.guess').value = 22;
+console.log(document.querySelector('.guess').value);
